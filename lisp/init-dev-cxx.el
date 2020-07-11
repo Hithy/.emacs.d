8b6954305cd3b5e9293dbf36a87c72a1cb31dbb6
胡尧 <huyao@huyaos-iMac-Pro.lan>
2020-07-11 09:52:57 +0800

    auto set cxx compile command

--- lisp/init-dev-cxx.el
+++ lisp/init-dev-cxx.el
@@ -13,6 +13,20 @@
   (add-hook 'objc-mode-hook 'load-ccls)
   )
 
+(add-hook 'c++-mode-hook
+          (lambda ()
+	    (unless (or (file-exists-p "makefile")
+		        (file-exists-p "CMakeLists.txt")
+		        (file-exists-p "Makefile"))
+              (let ((output-file-name (concat (file-name-sans-extension buffer-file-name) ".out")))
+                (set (make-local-variable 'compile-command)
+		     (concat "clang++ -std=c++17 "
+                             buffer-file-name
+                             " -o "
+                             output-file-name
+                             "; "
+                             output-file-name))))))
+
 (setq lsp-prefer-flymake nil)
 (setq-default flycheck-disabled-checkers '(c/c++-clang c/c++-cppcheck c/c++-gcc))
 
