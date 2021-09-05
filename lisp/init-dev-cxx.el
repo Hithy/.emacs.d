41a5137f9dead335ed8eafce6f18da47c14b95ef
Huyao <huyao0302@hotmail.com>
2021-09-05 22:42:29 +0800

    update compile command generate

--- lisp/init-dev-cxx.el
+++ lisp/init-dev-cxx.el
@@ -19,19 +19,81 @@
 
 (add-hook 'c-mode-common-hook 'google-set-c-style)
 
+(defun is_cmake_project ()
+  (let ((project-root (vc-root-dir)))
+    (if (eq project-root nil)
+        nil
+      (file-exists-p (concat project-root "CMakeLists.txt"))
+      )
+    )
+  )
+
+(defun gen_cmake_gen_proj_command ()
+  (if (my-executable-find "ninja")
+      "cmake -B build_ninja -G \"Ninja\" ."
+    "cmake -B build ."
+    )
+  )
+
+(defun gen_cmake_compile_command (target)
+  (concat
+   (if (file-exists-p (concat (vc-root-dir) "build_ninja"))
+       "cmake --build build_ninja --target "
+     "cmake --build build --target "
+     )
+   target
+   )
+  )
+
+(defun gen_cmake_run_command ()
+  (message "gen cmake run command")
+  (concat "cd "
+          (vc-root-dir)
+          " && "
+          (gen_cmake_gen_proj_command)
+          " && "
+          (gen_cmake_compile_command "all")
+          " && "
+          (gen_cmake_compile_command "run")
+          )
+  )
+
+(defun gen_normal_run_command ()
+  (message "gen normal run command")
+  (let ((output-file-name (concat (file-name-sans-extension buffer-file-name) ".out")))
+    (concat "clang++ -std=c++17 "
+            (prin1-to-string buffer-file-name)
+            " -o "
+            (prin1-to-string output-file-name)
+            " && "
+            (prin1-to-string output-file-name))
+    )
+  )
+
+(defun set_local_compile_command ()
+  (set (make-local-variable 'compile-command)
+       (if (is_cmake_project)
+           (gen_cmake_run_command)
+         (gen_normal_run_command)
+         )
+       )
+  )
+
+(defun cxx-run ()
+  (if (eq compile-command nil)
+      (set_local_compile_command)
+    )
+  (compile compile-command)
+  )
+
 (add-hook 'c++-mode-hook
           (lambda ()
-	    (unless (or (file-exists-p "makefile")
-		        (file-exists-p "CMakeLists.txt")
-		        (file-exists-p "Makefile"))
-              (let ((output-file-name (concat (file-name-sans-extension buffer-file-name) ".out")))
-                (set (make-local-variable 'compile-command)
-		     (concat "clang++ -std=c++17 "
-                             (prin1-to-string buffer-file-name)
-                             " -o "
-                             (prin1-to-string output-file-name)
-                             " && "
-                             (prin1-to-string output-file-name)))))))
+            (set (make-local-variable 'compile-command) nil)
+            (local-set-key (kbd "C-c c") (lambda ()
+                                           (interactive)
+                                           (cxx-run)
+                                           ))
+            ))
 
 (setq lsp-prefer-flymake nil)
 (setq-default flycheck-disabled-checkers '(c/c++-clang c/c++-cppcheck c/c++-gcc))
