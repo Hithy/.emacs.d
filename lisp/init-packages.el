a27fe15cc82f9e806abc10485ce574b01d18a788
yao <huyao0302@hotmail.com>
2022-10-11 10:04:36 +0800

    update elpa to sjtu

--- lisp/init-packages.el
+++ lisp/init-packages.el
@@ -2,10 +2,10 @@
 (provide 'init-packages)
 
 (require 'package)
-(setq package-archives '(("gnu"   . "https://mirrors.163.com/elpa/gnu/")
-			 ("melpa" . "https://mirrors.163.com/elpa/melpa/")
-			 ("org" . "https://mirrors.163.com/elpa/org/")
-			 ("melpa-stable" . "https://mirrors.163.com/elpa/melpa-stable/")))
+(setq package-archives '(("gnu"   . "https://mirrors.sjtug.sjtu.edu.cn/emacs-elpa/gnu/")
+			 ("melpa" . "https://mirrors.sjtug.sjtu.edu.cn/emacs-elpa/melpa/")
+			 ("org" . "https://mirrors.sjtug.sjtu.edu.cn/emacs-elpa/org/")
+			 ("melpa-stable" . "https://mirrors.sjtug.sjtu.edu.cn/emacs-elpa/stable-melpa/")))
 (setq package-archive-priorities
          '(("melpa-stable" . 20)
            ("org" . 10)
